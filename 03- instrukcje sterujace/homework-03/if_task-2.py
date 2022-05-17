6f38a05392f761c29460b28cc9c481ec0f97b2ab
milakow <kamila.kowalik97@gmail.com>
2022-05-17 22:11:23 +0200

    Changes

--- 03- instrukcje sterujace/homework-03/if_task-2.py
+++ 03- instrukcje sterujace/homework-03/if_task-2.py
@@ -1,1 +1,9 @@
-number1 = int(input('Podaj'))
+number1 = int(input('Podaj pierwszą liczbę: '))
+number2 = int(input('Podaj drugą liczbę: '))
+
+sum = number1 + number2
+
+if sum > 100:
+    print(sum)
+else:
+    print('Koniec')
--- 03- instrukcje sterujace/if_task-3_opinion.py
+++ 03- instrukcje sterujace/if_task-3_opinion.py
@@ -1,12 +1,12 @@
-opinion1 = int(input('Podaj opinię w skali od 1 do 10 o książce uwzględniając, że: ponad 7 - bardzo dobry, ocena 5-7 przeciętna, 4 i mniej - nie warta uwagi. Jaka jest twoja opinia?: '))
-opinion2 = int(input('Podaj opinię w skali od 1 do 10 o książce uwzględniając, że: ponad 7 - bardzo dobry, ocena 5-7 przeciętna, 4 i mniej - nie warta uwagi. Jaka jest twoja opinia?: '))
-opinion3 = int(input('Podaj opinię w skali od 1 do 10 o książce uwzględniając, że: ponad 7 - bardzo dobry, ocena 5-7 przeciętna, 4 i mniej - nie warta uwagi. Jaka jest twoja opinia?: '))
+opinion1 = int(input('Podaj opinię w skali 1- 10 o książce (>7 - bdb, ocena 5-7 przeciętna, <=4- nie warta uwagi): '))
+opinion2 = int(input('Podaj opinię w skali 1- 10 o książce (>7 - bdb, ocena 5-7 przeciętna, <=4- nie warta uwagi): '))
+opinion3 = int(input('Podaj opinię w skali 1- 10 o książce (>7 - bdb, ocena 5-7 przeciętna, <=4- nie warta uwagi): '))
 
 average_opinion = (opinion1 + opinion2 + opinion3) / 3
 
 if average_opinion > 7:
-    print ('Książka jest bardzo dobra.')
-elif average_opinion >=5:
-    print ('Książla jest przeciątna.')
+    print('Książka jest bardzo dobra.')
+elif average_opinion >= 5:
+    print('Książla jest przeciątna.')
 else:
     print('Książka nie jest warta uwagi.')
