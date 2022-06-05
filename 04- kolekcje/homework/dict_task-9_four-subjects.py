c4aa2561f4988d809e6bbd738833d596204167ef
milakow <kamila.kowalik97@gmail.com>
2022-06-05 18:09:46 +0200

    Changes

--- 04- kolekcje/homework/dict_task-9_four-subjects.py
+++ 04- kolekcje/homework/dict_task-9_four-subjects.py
@@ -1,7 +1,26 @@
-student_1 = input('Enter a list of four school subjexts separeted with comma: ')
-student_2 = input('Enter a list of four school subjexts separeted with comma: ')
-student_3 = input('Enter a list of four school subjexts separeted with comma: ')
-student_4 = input('Enter a list of four school subjexts separeted with comma: ')
+list_of_student_subjects = []
+for student in range(5):
+    subject = input('Enter a list of four school subjects separated with comma: ').replace(' ', '').lower().split(",")
+    list_of_student_subjects.extend(subject)
+# print(list_of_student_subjects)
 
-student_1 = student_1.split(",")
-print(student_1)
+set_of_subjects = set(list_of_student_subjects)
+# print(set_of_subjects)
+
+dict_of_subjects = dict.fromkeys(set_of_subjects, 0)
+# print(dict_of_subjects)
+
+for subject in set_of_subjects:
+    values = list_of_student_subjects.count(subject)
+    dict_of_subjects[subject] = values
+print(dict_of_subjects)
+
+max_val = max(dict_of_subjects, key=dict_of_subjects.get)
+print(f'The most common subject is {max_val}')
+
+
+
+
+
+
+
