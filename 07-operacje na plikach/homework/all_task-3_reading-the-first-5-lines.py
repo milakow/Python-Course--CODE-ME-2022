9bd56678cb0d318c50daf6b4a2d69f48ecc5a156
milakow <kamila.kowalik97@gmail.com>
2022-06-07 21:13:13 +0200

    Changes

---
+++ 07-operacje na plikach/homework/all_task-3_reading-the-first-5-lines.py
@@ -0,0 +1,12 @@
+def show_five_lines(filename):
+    with open(f'{filename}.txt', encoding='utf-8') as f:
+        content = f.readlines()
+        print(*content[0:5])
+
+
+def main():
+    filename = input('Enter the name of the file: ')
+    show_five_lines(filename)
+
+
+main()
